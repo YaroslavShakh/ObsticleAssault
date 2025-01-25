8221fc0cc9161bbea2c77ef9552279f70b1e3218
Ярослав Шах <162556173+YaroslavShakh@users.noreply.github.com>
2025-01-25 15:41:17 +0200

    Initial and Final commit
    Final version of project for repository

---
+++ Source/ObsticleAssault/MovingPlatform.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+   
+	StartLocation = GetActorLocation();
+	
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	 
+
+MovePlatform(DeltaTime);
+RotatePlatform(DeltaTime);	 
+}
+
+void AMovingPlatform:: MovePlatform(float DeltaTime)
+{
+  if(ShouldPlatformReturn())
+  {
+	FVector MovedDiraction = PlatformVelocity.GetSafeNormal();
+	StartLocation = StartLocation + MovedDiraction * RangeOfMoving;
+	SetActorLocation(StartLocation);
+	PlatformVelocity = -PlatformVelocity;
+  }
+
+  else
+  {
+	 FVector CurrentLocation = GetActorLocation();
+	  
+     CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
+	  SetActorLocation(CurrentLocation);
+  }
+}
+ void AMovingPlatform::RotatePlatform(float DeltaTime)
+ {
+   AddActorLocalRotation(RotationVelocity * DeltaTime);
+ }
+
+ 
+   bool AMovingPlatform:: ShouldPlatformReturn() const
+   {
+	return MovedDistance()>RangeOfMoving;
+   }
+
+   float AMovingPlatform:: MovedDistance() const
+   {
+    return FVector::Dist(GetActorLocation(),StartLocation);
+}
